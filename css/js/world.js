a485b30e8b0e5c27dc9d10ff96b5062fa6bbc9b8
Empresastar <gemadotopmatheus@gmail.com>
2026-03-19 21:21:22 -0300

    Refactor World class to use updated geometry and block color

--- css/js/world.js
+++ css/js/world.js
@@ -1,18 +1,18 @@
 import * as THREE from 'three';
-import { Block } from './block.js'; // PRECISA DO .JS NO FINAL
+import { Block } from './block.js';
 
 export class World {
     constructor(scene) {
         this.scene = scene;
-        this.blockGeometry = new THREE.BoxGeometry(1, 1, 1);
+        this.geometry = new THREE.BoxGeometry(1, 1, 1);
     }
 
     generate(size = 10) {
         for (let x = -size; x < size; x++) {
             for (let z = -size; z < size; z++) {
-                const block = new Block(x, 0, z, 'grass');
-                const material = new THREE.MeshLambertMaterial({ color: block.getTextureColor() });
-                const mesh = new THREE.Mesh(this.blockGeometry, material);
+                const blockInfo = new Block(x, 0, z, 'grass');
+                const material = new THREE.MeshLambertMaterial({ color: blockInfo.getColor() });
+                const mesh = new THREE.Mesh(this.geometry, material);
                 mesh.position.set(x, 0, z);
                 this.scene.add(mesh);
             }
