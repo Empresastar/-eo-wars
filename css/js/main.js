3c4871a3d8f4f0f285402f4e7b05dff8fd08538f
Empresastar <gemadotopmatheus@gmail.com>
2026-03-19 21:30:10 -0300

    Update main.js

--- css/js/main.js
+++ css/js/main.js
@@ -13,7 +13,6 @@ function init() {
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
     document.body.appendChild(renderer.domElement);
 
     const sun = new THREE.DirectionalLight(0xffffff, 1);
@@ -38,7 +37,6 @@ function animate() {
     requestAnimationFrame(animate);
     const time = performance.now();
     const delta = (time - prevTime) / 1000;
-
     player.update(delta);
     renderer.render(scene, camera);
     prevTime = time;
